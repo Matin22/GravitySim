610713852e869508e1456768164ec3692175a3f3
Matin22 <gholami.matin@gmail.com>
2025-08-12 22:41:16 +0200

    new shaders for model and projection

--- res/shaders/fShader.glsl
+++ res/shaders/fShader.glsl
@@ -1,11 +1,10 @@
 #version 330 core
 
-in vec2 TexCoords;
-out vec4 color;
+out vec4 FragColor;
 
-uniform sampler2D image;
-uniform vec3 spriteColor;
+uniform vec4 color;
 
-void main(){
-    color = vec4(spriteColor, 1.0) * texture(image, TexCoords);
-}
+void main()
+{    
+    FragColor = color;
+} 
