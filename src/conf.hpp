934ec1bc8e3dc19ac83a2fae23164444625725fa
Matin22 <gholami.matin@gmail.com>
2025-08-13 22:03:10 +0200

    constants and trail

--- src/conf.hpp
+++ src/conf.hpp
@@ -3,12 +3,23 @@
 namespace conf
 {
     // window configs
-    constexpr int SCREEN_WIDTH = 800;
-    constexpr int SCREEN_HEIGHT = 600;
+    constexpr int SCREEN_WIDTH = 1000;
+    constexpr int SCREEN_HEIGHT = 1000;
     constexpr int MAX_FRAMERATE = 144;
     constexpr float dt = 1.0f / static_cast<float>(MAX_FRAMERATE);
+
+    constexpr int MAX_TRAIL_LENGTH = 0;
 
     // constexpr float G_CONSTANT = 6.67430e-11;
     constexpr float G_CONSTANT = 1000.0f;
 
+    constexpr glm::vec2 OBJECT1_POSITION    = glm::vec2(conf::SCREEN_WIDTH / 2.0f, conf::SCREEN_HEIGHT / 2.0f);
+    constexpr float     OBJECT1_RADIUS      = 30.0f;
+    constexpr glm::vec2 OBJECT1_VELOCITY    = glm::vec2(0.0f, 0.0f);
+    constexpr glm::vec4 OBJECT1_COLOR       = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+    
+    constexpr glm::vec2 OBJECT2_POSITION    = glm::vec2(conf::SCREEN_WIDTH / 2.0f, conf::SCREEN_HEIGHT / 2.0f + 300.0f);
+    constexpr float     OBJECT2_RADIUS      = 10.0f;
+    constexpr glm::vec2 OBJECT2_VELOCITY    = glm::vec2(18.2f, 0.0f);
+    constexpr glm::vec4 OBJECT2_COLOR       = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
 }
